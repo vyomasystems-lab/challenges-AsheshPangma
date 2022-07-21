97c2bf3e810c8f37e877592d481f0579cd61e3b2
AsheshPangma <jotkoolimbu@gmail.com>
2022-07-21 20:37:10 +0000

    first commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -8,3 +8,15 @@ async def test_mux(dut):
     """Test for mux2"""
 
     cocotb.log.info('##### CTB: Develop your test here ########')
+
+    for i in range(31):
+
+        SEL = i
+
+        dut.sel.value = SEL
+
+        await Timer(2, units='ns')
+        
+        dut._log.info(f'SEL={(SEL):05} model={sel:05} DUT={int(dut.sel.value):05}')
+        assert dut.sel.value == SEL, "Randomised test failed with: {SEL}".format(
+            SEL=dut.sel.value)
