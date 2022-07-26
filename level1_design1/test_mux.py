344983280800d4c026e69432976ed4562e8fcca0
AsheshPangma <jotkoolimbu@gmail.com>
2022-07-26 11:19:16 +0000

    second commit

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -3,6 +3,8 @@
 import cocotb
 from cocotb.triggers import Timer
 import random
+from cocotb.binary import BinaryValue
+from cocotb.handle import _AssignmentResult
 
 @cocotb.test()
 async def test_mux(dut):
@@ -11,15 +13,17 @@ async def test_mux(dut):
     cocotb.log.info('##### CTB: Develop your test here ########')
     
     OUT = ["inp0", "inp1", "inp2", "inp3", "inp4", "inp5", "inp6", "inp7", "inp8", "inp9", "inp10", "inp11", "inp12", "inp13", "inp14", "inp15", "inp16", "inp17", "inp18", "inp19", "inp20", "inp21", "inp22", "inp23", "inp24", "inp25", "inp26", "inp27", "inp28", "inp29", "inp30"]
+   
+    
+    for i in range(31):
 
-    for i in range(0b11111):
-
-        SEL = i
-        dut.sel.value = SEL
+        A = BinaryValue(i, 5)
+        dut.sel.value = A
 
         await Timer(2, units='ns')
         
-        dut._log.info(f'SEL={SEL:05} model={OUT[i]} DUT={(dut.out)}')
-        assert dut.out == OUT[i], "Randomised test failed with: {SEL} = {OUT}".format(
-            SEL=dut.sel.value, OUT=dut.out)
+        if i == 30:
+            OUT[i] = "0"
+
+        dut._log.info(f'SEL={dut.sel.value} Model={OUT[i]} Actual={dut.out}')
        
--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -9,6 +9,8 @@ from pathlib import Path
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge
+from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
 
 @cocotb.test()
 async def test_seq_bug1(dut):
@@ -24,3 +26,13 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
+
+    inp_bit_test = [1,0,1,1,0,1,1,0,1,1,1,0,1,1,0,1,1,0,1,1]
+    dut.inp_bit.value = 0
+    dut.seq_seen.value = 0
+
+    for i in range(19):
+
+        dut.inp_bit.value = inp_bit_test[i]
+        await Timer(10, units='us')
+        dut._log.info(f'inputbits={dut.inp_bit.value} seqseen={dut.seq_seen.value}')
