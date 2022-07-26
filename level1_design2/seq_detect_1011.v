ed198a46e036fca6625e152c9f32a2988dcaffbd
AsheshPangma <jotkoolimbu@gmail.com>
2022-07-26 11:22:11 +0000

    second commit

--- level1_design2/seq_detect_1011.v
+++ level1_design2/seq_detect_1011.v
@@ -66,10 +66,7 @@ module seq_detect_1011(seq_seen, inp_bit, reset, clk);
       end
       SEQ_1011:
       begin
-        if(inp_bit == 1)
-          next_state = IDLE;
-        else
-          next_state = SEQ_10;
+        next_state = IDLE;
       end
     endcase
   end
